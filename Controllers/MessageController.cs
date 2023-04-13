e85ffda76720eb657858a49ed7e14629ca601fdf
RobertoMK <robertomendes28@hotmail.com>
2023-04-13 10:45:49 -0300

    <fix>: headless mode now uses local chromium session

--- Controllers/MessageController.cs
+++ Controllers/MessageController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using PuppeteerSharp;
+using System;
 using WhatsappAPI.Models;
 
 namespace WhatsappAPI.Controllers
@@ -20,30 +21,34 @@ namespace WhatsappAPI.Controllers
         [HttpPost]
         public async void Send(Message m)
         {
-            //string[] args = new string ["--disable-gpu", "--disable-setuid-sandbox", "--no-sandbox", "--no-zygote"];
-
             var parameters = "";
             using var browserFetcher = new BrowserFetcher();
-            await browserFetcher.DownloadAsync(BrowserFetcher.DefaultChromiumRevision);
-            var chromePath = browserFetcher.GetExecutablePath(BrowserFetcher.DefaultChromiumRevision);
+            var chromePath = browserFetcher.DownloadAsync(BrowserFetcher.DefaultChromiumRevision).Result;
 
-            LaunchOptions launchOptions = new LaunchOptions { Headless = true, UserDataDir = "./tmp", Args = new string[] { "--disable-setuid-sandbox", "--no-sandbox", "--no-zygote" } };
+            LaunchOptions launchOptions = new LaunchOptions { Headless = true, UserDataDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ".local-chromium", "Win64-884014", "chrome-win", "UserData").Replace(@"\", @"\\"), ExecutablePath = chromePath.ExecutablePath };
 
-            var browser = Puppeteer.LaunchAsync(launchOptions).Result;
+            await using var browser = await Puppeteer.LaunchAsync(launchOptions);
             Thread.Sleep(1000);
             parameters = string.Concat(url, m.number, "&text=", Uri.EscapeDataString(m.message));
 
             //puppeteer script start --------------------------------------------
             var page = await browser.NewPageAsync();
             await page.GoToAsync(parameters);
+            try
+            {
+                var sendMessage = page.WaitForSelectorAsync("[data-testid=\"conversation-compose-box-input\"]").Result;
+                Thread.Sleep(1000);
+                await page.ClickAsync("[data-testid=\"conversation-compose-box-input\"]");
+                Thread.Sleep(100);
+                await page.Keyboard.PressAsync("Enter");
+                Thread.Sleep(1000);
+                await browser.CloseAsync();
+            } catch (WaitTaskTimeoutException ex)
+            {
+                //await page.ScreenshotAsync();
+                Console.WriteLine(ex.ToString());
+            }
 
-            var sendMessage = page.WaitForSelectorAsync("[data-testid=\"conversation-compose-box-input\"]").Result;
-            Thread.Sleep(1000);
-            await page.ClickAsync("[data-testid=\"conversation-compose-box-input\"]");
-            Thread.Sleep(100);
-            await page.Keyboard.PressAsync("Enter");
-            Thread.Sleep(100);
-            await browser.CloseAsync();
             //puppeteer script end --------------------------------------------
         }
     }
---
+++ Helpers/AuthHelper.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace WhatsappAPI.Helpers
+{
+    public class AuthHelper
+    {
+        private readonly RequestDelegate _next;
+
+        public AuthHelper(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+
+            return _next(httpContext);
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<AuthHelper>();
+        }
+    }
+}
